e4ebe0d470506682e15d5a7cdc1ebf026664c6d6
schmitzjr <schmitzjr@gmail.com>
2022-03-14 23:56:49 -0300

    Changes

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,58 +1,54 @@
 <template>
-    <div class="formulario">
+  <div class="formulario">
+    <div class="fomulario-title">{{ title }}</div>
 
-        <div class="fomulario-title">{{title}}</div>
-        
-        <input type="text" class="formulario-input" v-model="inputValue" />
+    <input type="text" class="formulario-input" v-model="inputValue" />
 
-        <button 
-            :disabled="inputValue.length === 0"
-            v-on:click="onSubmit">{{button}}</button>
-
-    </div>
+    <button :disabled="inputValue.length === 0" v-on:click="onSubmit">
+      {{ button }}
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'FormularioComp',
-    props: {
-        title: String,
-        button: String,
-        action: Function
+  name: "FormularioComp",
+  props: {
+    title: String,
+    button: String,
+    action: Function,
+  },
+  data() {
+    return {
+      inputValue: "",
+    };
+  },
+  methods: {
+    onSubmit: function () {
+      this.action(this.inputValue);
+      this.inputValue = "";
     },
-    data() {
-        return {
-            inputValue:''
-        }
-    },
-    methods: {
-        onSubmit: function(){
-            this.action(this.inputValue);
-            this.inputValue = '';
-        }
-    },
-}
+  },
+};
 </script>
 
 <style>
-
-.formulario{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
-    justify-content: center;
-    margin-top: 20px;
+.formulario {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  margin-top: 20px;
 }
 
-.formulario-input{
-    padding: 10px 5px;
-    border: 0;
-    border-radius: 5px;
-    margin: 10px 0px;
-    text-align: center;
-    color: var(--color-text-dark);
-    font-size: 16px;
+.formulario-input {
+  padding: 10px 5px;
+  border: 0;
+  border-radius: 5px;
+  margin: 10px 0px;
+  text-align: center;
+  color: var(--color-text-dark);
+  font-size: 16px;
 }
-    
 </style>
